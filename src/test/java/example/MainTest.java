6516228e00068110f5eabaddac9729c871c9d00a
sabarna1604 <sabarnasubramani@gmail.com>
2018-01-20 19:10:04 +0530

    first commit

---
+++ src/test/java/example/MainTest.java
@@ -0,0 +1,29 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MainTest {
+	private WebDriver driver;
+  @Test
+  public void f() {
+	  driver.get("http://google.in");
+	  String title = driver.getTitle();      
+	  Assert.assertTrue(title.equals("Google"));   
+  }
+  
+  @BeforeTest  
+  public void beforeTest() {    
+     
+    driver = new FirefoxDriver();    
+  }      
+  @AfterTest  
+  public void afterTest() {  
+    driver.quit();        
+    
+  }   
+}
